5847b94c1bbb588ebba217cb37a6eae314145bf3
darriola12 <132181804+darriola12@users.noreply.github.com>
2024-08-02 19:33:59 +0000

    cambios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Reemplaza fetch con axios
+    axios.get(`${import.meta.env.VITE_API_URL}/api/test`)
+      .then(response => {
+        console.log(response); // Verifica la respuesta completa
+        setData(response.data); // Axios ya convierte la respuesta a JSON
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>React and Node.js Integration</h1>
+      {data ? <p>{data.message}</p> : <p>Loading...</p>}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- server/src/app.js
+++ server/src/app.js
@@ -3,23 +3,21 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Rutas básicas
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.use((err, req, res, next) => {
-    console.error(err.stack); // Imprime el stack trace del error en la consola
-    res.status(500).send('Something broke!'); // Envía una respuesta con un estado 500 y un mensaje de error
+// Ruta de prueba
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Hello from the backend!' });
 });
 
 // Iniciar el servidor
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-});
+}); 
+
+
+
