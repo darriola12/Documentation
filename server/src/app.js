d4a9843c7b9794a9aefc37fb3965cdd6d6c3a9e1
darriola12 <132181804+darriola12@users.noreply.github.com>
2024-08-02 17:36:04 +0000

    cambios

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,25 @@
-import express from 'express';
-import cors from 'cors';
-import tieRoutes from './routes/tieRoutes.js';
-import dotenv from 'dotenv';
-
-dotenv.config();
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
 app.use(cors());
-app.use(express.json()); // Para parsear JSON
+app.use(express.json());
 
-app.use('/api', tieRoutes); // Usar las rutas definidas
+// Rutas básicas
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use((err, req, res, next) => {
+    console.error(err.stack); // Imprime el stack trace del error en la consola
+    res.status(500).send('Something broke!'); // Envía una respuesta con un estado 500 y un mensaje de error
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
